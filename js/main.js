b44d3ebe545c07c0fb57cea43a2f138e1752766e
unknown <agm.398@gmail.com>
2020-12-11 15:11:41 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/*var nome = "Anderson Moura";
+var idade=25;
+var n1=5;
+var n2=3;
+var frase="Japão é o melhor time do mundo";
+//alert(nome + " tem "+ idade + " anos");
+console.log(frase.replace("Japão","Brasil"));
+console.log(n1*n2);
+alert(frase.toLowerCase());*/
+
+/*var lista = ["maça", "pêra", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista.lenght);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));*/
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);*/
+
+/*var idade = prompt("Qual a sua idade?");
+if (idade>=18){
+    alert("Maior de idade");
+}
+else{
+    alert("Menor de idade");
+};*/
+
+var d = new Date();
+alert(d.getDate());
+alert(d.getHours());
+alert(d.getMinutes());
+alert(d.getMonth()+1);
